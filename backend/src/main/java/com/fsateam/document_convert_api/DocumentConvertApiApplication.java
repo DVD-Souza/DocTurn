2cbd75aafcf721cce5fb5d63236140c5d3a57625
Deivid <deivid@Deivid.Souza>
2026-07-10 21:03:27 -0300

    pushing the base structure for the document conversion API

---
+++ backend/src/main/java/com/fsateam/document_convert_api/DocumentConvertApiApplication.java
@@ -0,0 +1,13 @@
+package com.fsateam.document_convert_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentConvertApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentConvertApiApplication.class, args);
+	}
+
+}
