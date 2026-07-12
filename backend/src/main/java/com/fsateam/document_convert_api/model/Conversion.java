d7d5000148444d9b0ab6974cbc4f753b76d3ea43
Deivid <deivid@Deivid.Souza>
2026-07-11 21:26:18 -0300

    adding the implementation of the conversion class.

--- backend/src/main/java/com/fsateam/document_convert_api/model/Conversion.java
+++ backend/src/main/java/com/fsateam/document_convert_api/model/Conversion.java
@@ -1,4 +1,32 @@
 package com.fsateam.document_convert_api.model;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Conversion {
+    private Long id;
+    @NonNull
+    private File sourceDocument;
+    @NonNull
+    private File targetDocument;
+    @NonNull
+    private LocalDateTime startedAt;
+    @NonNull
+    private LocalDateTime finishedAt;
+
+    private enum ConversionType{
+        PDF,
+        DOCX
+    }
+
+    private enum Status{
+        IN_PROGRESS,
+        COMPLETE,
+        FAILED
+    }
 }
