d27c7b9ad4f701e1384e852ab5bb691732fe838a
Joao <jv425445@gmail.com>
2026-07-11 21:41:06 -0300

    add file class anda controller package

--- backend/src/main/java/com/fsateam/document_convert_api/model/File.java
+++ backend/src/main/java/com/fsateam/document_convert_api/model/File.java
@@ -1,4 +1,24 @@
 package com.fsateam.document_convert_api.model;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 
 public class File {
+    private long id;
+    @NonNull
+    private String name;
+    @NonNull
+    private String type;
+    @NonNull
+    private long size;
+    @NonNull
+    private FileFormat format;
+    @NonNull
+    private Path path;
+    @NonNull
+
+
 }
