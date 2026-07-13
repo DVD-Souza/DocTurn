cfa8cc2c019db26b48541a1e08a1e708d1ceeacb
Joao <jv425445@gmail.com>
2026-07-13 20:05:03 -0300

    add FileController

--- backend/src/main/java/com/fsateam/document_convert_api/controller/FileController.java
+++ backend/src/main/java/com/fsateam/document_convert_api/controller/FileController.java
@@ -1,4 +1,28 @@
 package com.fsateam.document_convert_api.controller;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Controller
+@RequestMapping(path = "/converter")
 public class FileController {
+
+    private final FileService service;
+
+
+    public FileController(){
+        this.service = new FileService();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> FileConversion(@RequestParam("File") MultipartFile file) {
+       return null;
+    };
+
 }
